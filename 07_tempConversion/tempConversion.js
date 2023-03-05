31686ec11c6c62e9ddd2bf7c3dbf65420157c8aa
31miR <76799355+31miR@users.noreply.github.com>
2023-03-05 03:04:24 +0100

    Finish exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(x) {
+  return Math.round((x-32)*5*10/9)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(x) {
+  return Math.round((9*x/5 + 32)*10)/10;
 };
 
 // Do not edit below this line
