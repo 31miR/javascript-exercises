608dbc3f3348caa7d06701d68847015604f113df
31miR <76799355+31miR@users.noreply.github.com>
2023-03-05 02:50:28 +0100

    Finish exercises 1-5 (inclusive)

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...toDelete) {
+    for(a of toDelete){
+        for(let i = 0; i < arr.length; ++i){
+            if(a === arr[i]) arr.splice(i,1);
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if ( (typeof a != "number") || (typeof b != "number") ) return "ERROR";
+    if ( (a < 0) || (b < 0) ) return "ERROR";
+    if (a > b) {
+        let k = a;
+        a = b;
+        b = k;
+    }
+    return (a+b)*(b-a+1)/2;
 };
 
 // Do not edit below this line
